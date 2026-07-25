c072c3030a0ce683c71e5b52a4e86ff25d5179a2
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-07-25 18:00:11 +0530

    building products page

--- frontend/src/landing_page/products/Hero.jsx
+++ frontend/src/landing_page/products/Hero.jsx
@@ -1,8 +1,19 @@
 function Hero() {
-    return ( 
+    return (
+      <div className="container p-5 mb-5">
 
-        <h1>hero products</h1>
-     );
+        <div className="row text-center">
+          <h1 className="mt-5 fs-2">TradeNova Products</h1>
+          <p className="fs-5 p-2">
+            Sleek, modern, and intuitive trading platforms
+          </p>
+          <p style={{fontSize:"18px"}}>
+            Check out our <a href="" style={{textDecoration:"none"}}>investment offerings →</a>
+          </p>
+        </div>
+
+      </div>
+    );
 }
 
 export default Hero;
