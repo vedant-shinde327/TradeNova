73728d8f59f2fa375c6eb13d4974b2ca762f0bf1
vedant-shinde327 <vedantbaban.shinde@gmail.com>
2026-07-30 11:15:48 +0530

    pricing page completed

--- frontend/src/landing_page/pricing/Brokrage.jsx
+++ frontend/src/landing_page/pricing/Brokrage.jsx
@@ -1,7 +1,47 @@
 function Brokrage() {
-    return ( 
-        <h1>Brokrage</h1>
-     );
+  return (
+    <div className="container">
+      <div className="row p-5 text-center border-top">
+        <div className="col-8  p-4 mt-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5 p-3">Brokrage calculator</h3>
+          </a>
+          <ul
+            className="text-muted"
+            style={{ lineHeight: "2.3", textAlign: "left", fontSize:"14px" }}
+          >
+            <li>
+              Call & Trade and RMS auto-squareoff; additional charges of ₹50 +
+              GST per order.
+            </li>
+            <li>Digital contract notes will be sent via email.</li>
+            <li>
+              Physical copies of contract notes, if required, shall be charged
+              ₹20 per contract note. Courier charges apply.
+            </li>
+            <li>
+              for NRI account (non-PIS), 0.5% or ₹100 per executed order for
+              equity (whichever is lower).
+            </li>
+            <li>
+              for NRI account (PIS), 0.5% or ₹200 per executed order for equity
+              (whichever is lower).
+            </li>
+            <li>
+              if the account is in debit balance, any order placed will be
+              charged ₹40 per executed order isntead of ₹20 per executed order.
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-4 p-4 mt-4">
+          <a href="" style={{ textDecoration: "none" }}>
+            <h3 className="fs-5 p-3">List of charges</h3>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Brokrage;
--- frontend/src/landing_page/pricing/Hero.jsx
+++ frontend/src/landing_page/pricing/Hero.jsx
@@ -1,55 +1,47 @@
+import zeroImg from "../../assets/images/pricingEquity.svg";
+import TwoZeroImg from "../../assets/images/intradayTrades.svg";
+
 function Hero() {
-    return (
-      <div className="container">
-        <div className="row p-5  border-bottom fs-6 ">
-          <h1 className="fs-2 text-center pt-4 mt-3">Charges</h1>
-          <p className="text-center text-muted pt-2 mb-5 fs-5">
-            List of all charges and taxes
+  return (
+    <div className="container">
+      <div className="row p-5 fs-6 ">
+        <h1 className="fs-2 text-center pt-4 mt-3">Charges</h1>
+        <p className="text-center text-muted pt-2 fs-4">
+          List of all charges and taxes
+        </p>
+      </div>
+
+      <div className="row p-5 text-center">
+        <div className="col-4  p-4">
+          <img src={zeroImg} />
+          <h3>Free equity delivery</h3>
+          <p className="text-muted" style={{ fontSize: "17px" }}>
+            All equity delivery investments (NSE, BSE), are absolutely free — ₹
+            0 brokerage.
           </p>
-          <div className="col-6 p-5">
-            <p style={{ fontSize: "17px" }}>
-              We kick-started operations on the 15th of August, 2010 with the
-              goal of breaking all barriers that traders and investors face in
-              India in terms of cost, support, and technology. We named the
-              company Zerodha, a combination of Zero and "Rodha", the Sanskrit
-              word for barrier.
-            </p>
+        </div>
 
-            <p style={{ fontSize: "17px" }}>
-              Today, our disruptive pricing models and in-house technology have
-              made us the biggest stock broker in India.
-            </p>
+        <div className="col-4 p-4">
+          <img src={TwoZeroImg} />
+          <h3>Intraday and F&O trades</h3>
+          <p className="text-muted" style={{ fontSize: "17px" }}>
+            Flat ₹ 20 or 0.03% (whichever is lower) per executed order on
+            intraday trades across equity, currency, and commodity trades. Flat
+            ₹20 on all option trades.
+          </p>
+        </div>
 
-            <p style={{ fontSize: "17px" }}>
-              Over 1.6+ crore clients place billions of orders every year
-              through our powerful ecosystem of investment platforms,
-              contributing over 15% of all Indian retail trading volumes.
-            </p>
-          </div>
-          <div className="col-6 p-5">
-            <p style={{ fontSize: "17px" }}>
-              In addition, we run a number of popular open online educational
-              and community initiatives to empower retail traders and investors.
-            </p>
-
-            <p style={{ fontSize: "17px" }}>
-              <a href="" style={{ textDecoration: "none" }}>
-                Rainmatter
-              </a>
-              , our fintech fund and incubator, has invested in several fintech
-              startups with the goal of growing the Indian capital markets.
-            </p>
-
-            <p style={{ fontSize: "17px" }}>
-              And yet, we are always up to something new every day. Catch up on
-              the latest updates on our blog or see what the media is saying
-              about us or learn more about our business and product
-              philosophies.
-            </p>
-          </div>
+        <div className="col-4  p-4">
+          <img src={zeroImg} />
+          <h3>Free direct MF</h3>
+          <p className="text-muted" style={{ fontSize: "17px" }}>
+            All direct mutual fund investments are absolutely free — ₹ 0
+            commissions & DP charges.
+          </p>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Hero;
